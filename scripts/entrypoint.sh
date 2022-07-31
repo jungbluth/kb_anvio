35ddf4bfc7893db61606d3cf18285ebcae6f833a
Sean Jungbluth <jungbluth.sean@gmail.com>
2022-07-30 21:06:10 -0700

    prep for KBase ui test

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -25,18 +25,19 @@ elif [ "${1}" = "init" ] ; then
   # echo "Running anvi-setup-scg-taxonomy"
   # anvi-setup-scg-taxonomy -T 1
   
-  echo "Running anvi-setup-ncbi-cogs" # need BLAST tools
+  echo "Running anvi-setup-ncbi-cogs"
   anvi-setup-ncbi-cogs -T 4 --just-do-it --cog-data-dir /data/anviodb/COG
   # echo "Running anvi-setup-pfams" 
   # anvi-setup-pfams --pfam-data-dir /data/anviodb/Pfam
-  #echo "Running anvi-setup-kegg-kofams" # yaml update
-  #anvi-setup-kegg-kofams --download-from-kegg --kegg-data-dir /data/anviodb/KEGG
+  echo "Running anvi-setup-kegg-kofams" # yaml update
+  anvi-setup-kegg-kofams --download-from-kegg --kegg-data-dir /data/anviodb/KEGG
   # echo "anvi-setup-interacdome"
   # anvi-setup-interacdome --interacdome-data-dir /data/anviodb/Interacdome
   # echo "anvi-setup-pdb-database"
   # anvi-setup-pdb-database -T 1 --pdb-database-path /data/anviodb/PDB.db
   cd /data/anviodb
-  if [ -d "/data/anviodb/COG/COG20" -a -s "/data/anviodb/COG/COG20/DB_DIAMOND/COG.dmnd" -a -s "/data/anviodb/KEGG/MODULES.db" -a -s "/data/anviodb/KEGG/HMMs/Kofam.hmm" ] ; then
+  #if [ -d "/data/anviodb/COG/COG20" -a -s "/data/anviodb/COG/COG20/DB_DIAMOND/COG.dmnd" -a -s "/data/anviodb/KEGG/MODULES.db" -a -s "/data/anviodb/KEGG/HMMs/Kofam.hmm" ] ; then
+  if [ -d "/data/anviodb/COG/COG20" -a -s "/data/anviodb/COG/COG20/DB_DIAMOND/COG.dmnd" ] ; then
     echo "DATA DOWNLOADED SUCCESSFULLY"
     touch /data/__READY__
   else
